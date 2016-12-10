375d9dd82496836f4ba577fe1b43cda86076a9d7
srei <steferu@gmail.com>
2016-12-09 19:32:49 -0800

    edited .jade in order to render information dynamically

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+var readme = [
+  {
+    sectionTitle: 'Features',
+    sectionContent: `<ul>
+    <li>Here are some <strong>Really cool</strong></li>
+    <li>Awesome features</li>
+    </ul>`,
+  },
+  {
+    sectionTitle: 'Getting Started',
+    sectionContent: `<h2>Do Code stuff</h2>
+    <code>This is a code block</code>
+    <h2>Do Other code stuff</h2>
+    <code>This is the other code block</h2>`,
+  },
+  {
+    sectionTitle: 'How to Install',
+    sectionContent: `<h2>Do Code stuff</h2>
+    <code>This is a code block</code>
+    <h2>Do Other code stuff</h2>
+    <code>This is the other code block</h2>`,
+  },
+];
 
 let express = require('express'),
 		app = express(),
@@ -5,11 +28,18 @@ let express = require('express'),
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
+
 app.set('view engine', 'jade');
+
 app.set('views', __dirname + "/views");
 
 app.get('/', (req, res, next) => {
-	res.render('index')
+	res.render('index', {readme})
+});
+
+
+app.get('/duck', (req,res, next) => {
+	res.sendFile(__dirname + '/icons/duck.jpg');
 });
 
 const PORT = 3000;

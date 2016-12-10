223eaf1c4f57fdf21cc6a14c3a2ac751efe769f1
srei <steferu@gmail.com>
2016-12-10 14:59:58 -0800

    integrated raw github information into '/' route, added .then return in order to have access to data within .jade template page

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
-var readme = [
-  {
-    sectionTitle: 'Features',
-    sectionContent: `<ul>
-    <li>Here are some <strong>Really cool</strong></li>
-    <li>Awesome features</li>
-    </ul>`,
-  },
-  {
-    sectionTitle: 'Getting Started',
-    sectionContent: `<h2>Do Code stuff</h2>
-    <code>This is a code block</code>
-    <h2>Do Other code stuff</h2>
-    <code>This is the other code block</h2>`,
-  },
-  {
-    sectionTitle: 'How to Install',
-    sectionContent: `<h2>Do Code stuff</h2>
-    <code>This is a code block</code>
-    <h2>Do Other code stuff</h2>
-    <code>This is the other code block</h2>`,
-  },
-];
-
 let express = require('express'),
 		app = express(),
 		bodyParser = require('body-parser');
 
+//get marked, run marked on the strings in readme array//w/ what marked returns
+let fetch = require('node-fetch');
+fetch.promise = require('bluebird');
+
 app.use(bodyParser.json());
+
 app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'jade');
@@ -34,9 +15,36 @@ app.set('view engine', 'jade');
 app.set('views', __dirname + "/views");
 
 app.get('/', (req, res, next) => {
-	res.render('index', {readme})
+fetch('https://raw.githubusercontent.com/reduxify/seedux/master/README.md')
+    .then(response => response.text())
+    .then(text => {
+      let splitString = text.split('# Features');
+      splitString = splitString[1].split('# Instructions');
+      const featuresContent = splitString[0];
+      splitString = splitString[1].split('## Getting Started:');
+      const installContent = splitString[0];
+      const gettingStartedContent = splitString[1].split('## Complete Example Integration')[0];
+      return readme = [
+        {
+          sectionTitle: 'Features',
+          sectionContent: featuresContent,
+        },
+        {
+          sectionTitle: 'Install',
+          sectionContent: installContent,
+        },
+        {
+          sectionTitle: 'Getting Started',
+          sectionContent: gettingStartedContent,
+        },
+      ];
+    })
+    .then(readme => {
+    	console.log({readme});
+    	res.render('index', {readme});
+    })
+    .catch(err => console.log(err));
 });
-
 
 app.get('/duck', (req,res, next) => {
 	res.sendFile(__dirname + '/icons/duck.jpg');

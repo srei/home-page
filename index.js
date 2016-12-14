7c9f974c3dbdfd53cd3de27f308f7d001fd1ef57
srei <steferu@gmail.com>
2016-12-13 16:05:19 -0800

    changed duck icon to seedux icon, changed styling on buttons, changed font size

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ fetch('https://raw.githubusercontent.com/reduxify/seedux/master/README.md')
 });
 
 app.get('/duck', (req,res, next) => {
-	res.sendFile(__dirname + '/icons/duck.jpg');
+	res.sendFile(__dirname + '/icons/seedux.png');
 });
 
 const PORT = 3000;
